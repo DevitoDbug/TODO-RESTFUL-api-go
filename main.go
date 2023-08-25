c8418cf1634695be1ccf5e4ea2b9f8b016f59bad
DevitoDbug <Davidochiengy@gmail.com>
2023-08-25 22:08:10 +0300

    Implemented the getTask and the getTasks handlers

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 type Task struct {
+	ID          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Status      string `json:"status"` //status can be To Do , In Progress , Done
@@ -14,9 +17,31 @@ type Task struct {
 
 var tasks []Task
 
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	err := json.NewEncoder(w).Encode(tasks)
+	if err != nil {
+		log.Printf("%v\n", err)
+		return
+	}
+}
+
 func getTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
+	params := mux.Vars(r)
+
+	for _, task := range tasks {
+		if task.ID == params["id"] {
+			err := json.NewEncoder(w).Encode(task)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+				return
+			}
+			return
+		}
+	}
 }
 func deleteTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
@@ -30,21 +55,17 @@ func updateTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
 }
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-
-}
 
 func main() {
 	port := "8080"
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/tasks", getTasks)
-	r.HandleFunc("/tasks/{id}", getTask)
-	r.HandleFunc("/tasks", createTask)
-	r.HandleFunc("/tasks/{id}", deleteTask)
-	r.HandleFunc("/tasks/{id}", updateTask)
+	r.HandleFunc("/tasks", getTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	r.HandleFunc("/tasks", createTask).Methods("POST")
+	r.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	r.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
 
 	log.Printf("Starting server at port %v", port)
 
