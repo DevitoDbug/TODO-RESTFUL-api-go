c67b88b85b000c56b65e75e75ca226409c83de1f
DevitoDbug <Davidochiengy@gmail.com>
2023-08-25 22:01:03 +0300

    Created the handler functions

--- main.go
+++ main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 )
 
-var Task struct {
+type Task struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Status      string `json:"status"` //status can be To Do , In Progress , Done
+}
+
+var tasks []Task
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+}
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+}
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+}
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+}
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
 }
 
 func main() {
@@ -23,5 +47,11 @@ func main() {
 	r.HandleFunc("/tasks/{id}", updateTask)
 
 	log.Printf("Starting server at port %v", port)
+
+	err := http.ListenAndServe(port, r)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+		return
+	}
 
 }
